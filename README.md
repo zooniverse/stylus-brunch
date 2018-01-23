e3a40cbb412f44d55a9f2b6235d81465355feef8
Adam McMaster <adam@zooniverse.org>
2018-01-23 17:03:16 +0000

    Port forked code from Radio Galaxy Zoo

--- README.md
+++ README.md
@@ -1,151 +1,69 @@
-# stylus-brunch
-
+## stylus-brunch
 Adds [Stylus](http://learnboost.github.com/stylus/) support to
-[Brunch](http://brunch.io).
+[brunch](http://brunch.io).
 
-The plugin includes [nib](http://tj.github.io/nib/) cross-browser mixins.
+Also includes [nib](http://visionmedia.github.com/nib/) cross-browser mixins.
 
 ## Usage
-
-Install the plugin via npm with `npm install --save-dev stylus-brunch`.
+Install the plugin via npm with `npm install --save stylus-brunch`.
 
 Or, do manual install:
 
-* Add `"stylus-brunch": "x.y"` to `package.json` of your brunch app.
+* Add `"stylus-brunch": "x.y.z"` to `package.json` of your brunch app.
   Pick a plugin version that corresponds to your minor (y) brunch version.
 * If you want to use git version of plugin, add
-`"stylus-brunch": "github:brunch/stylus-brunch"`.
+`"stylus-brunch": "git+ssh://git@github.com:brunch/stylus-brunch.git"`.
 
 ## Options
-
-**You don't need to specify any options by default.**
-
 ### Use Plugin Middleware
-
 You can include Stylus plugins with a config directive
-`config.plugins.stylus.plugins` (array) with paths to require the needed
-plugins.  You will have to include your plugin dependencies in `package.json`.
+`config.plugins.stylus.plugins` (array) with paths to require the needed 
+plugins.  You will have to include your plugin dependencies in ```package.json```.
 
-```js
-module.exports = {
-  // ...
-  plugins: {
-    stylus: {
+```coffeescript
+config =
+  plugins:
+    stylus:
       plugins: ['my-stylus-plugin']
-    }
-  }
-};
 ```
-
-If the plugin is module based you can import a specific member as a subarray.
-
-```js
-moduls.exports = {
-  // ...
-  plugins: {
-    stylus: {
-      plugins: ['my-stylus-plugin', ['my-module-plugin', 'member']]
-    }
-  }
-};
-```
-
-Alternatively, you can pass a function.
-
-```js
-moduls.exports = {
-  // ...
-  plugins: {
-    stylus: {
-      plugins: [require('autoprefixer-stylus')({browsers: ['last 3 versions']})]
-    }
-  }
-};
-```
-
 
 ### Options
-
 You can import your modules or Stylus sheets with a config directive
 `config.plugins.stylus.imports` (array) with paths to your modules.
 
-```js
-moduls.exports = {
-  // ...
-  plugins: {
-    stylus: {
+```coffeescript
+config =
+  plugins:
+    stylus:
       imports: ['']
-    }
-  }
-};
 ```
 
 Allow stylus files to include plain-css partials:
 
-```js
-moduls.exports = {
-  // ...
-  plugins: {
-    stylus: {
+```coffeescript
+config =
+  plugins:
+    stylus:
       includeCss: true
-    }
-  }
-};
 ```
 
 ### Debugging
-
 Enable line number comments or FireStylus for Firebug debug messages (both are off by default)
 
-```js
-moduls.exports = {
-  // ...
-  plugins: {
-    stylus: {
-      linenos: true,
+```coffeescript
+config =
+  plugins:
+    stylus:
+      linenos: true
       firebug: true
-    }
-  }
-};
 ```
 
-### CSS Modules
-
-Starting Brunch `<unreleased>`, you can use CSS Modules with stylus-brunch. To enable it, change your config to:
-
-```js
-module.exports = {
-  // ...
-  plugins: {
-    stylus: {
-      modules: true
-    }
-  }
-};
-```
-
-Then, author your styles like you normally would:
-
-```stylus
-.title
-  font-size: 32px
-```
-
-And reference CSS class names by requiring the specific style into your javascript:
-
-```js
-var style = require('./title.styl');
-
-<h1 className={style.title}>Yo</h1>
-```
-
-Note: enabling `cssModules` does so for every stylesheet in your project, so it's all-or-nothing. Even the files you don't require will be transformed into CSS modules (aka will have obfuscated class names, like turn `.title` into `._title_fdphn_1`).
 
 ## License
 
 The MIT License (MIT)
 
-Copyright (c) 2012-2017 Paul Miller (http://paulmillr.com)
+Copyright (c) 2012-2013 Paul Miller (http://paulmillr.com)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- index.js
+++ index.js
@@ -1,122 +1,66 @@
 'use strict';
 
-const sysPath = require('path');
-const stylus = require('stylus');
-const nib = require('nib');
-const progeny = require('progeny');
+var sysPath = require('path');
+var stylus = require('stylus');
+var nib = require('nib');
+var progeny = require('progeny');
 
-const postcss = require('postcss');
-const postcssModules = require('postcss-modules');
-
-const cssModulify = (path, data, map) => {
-  let json = {};
-  // eslint-disable-next-line
-  const getJSON = (_, _json) => json = _json;
-
-  return postcss([postcssModules({getJSON})]).process(data, {from: path, map}).then(x => {
-    const exports = `module.exports = ${JSON.stringify(json)};`;
-    return {
-      exports,
-      data: x.css,
-      map: x.map,
-    };
+function StylusCompiler(cfg) {
+  if (cfg == null) cfg = {};
+  this.rootPath = cfg.paths.root;
+  this.config = (cfg.plugins && cfg.plugins.stylus) || {};
+  this.getDependencies = progeny({
+    rootPath: this.rootPath
   });
-};
-
-const re = /(([./\w]+)((:\d+){2}))/;
-const getError = error => {
-  error.name = '';
-  const str = error.toString();
-  const match = str.match(re);
-  const text = str.replace(match[1], '');
-  const msg = `L${match[3].slice(1)} ${text}`;
-  const err = new Error(msg);
-  err.name = '';
-  err.stack = error.stack;
-  return err;
-};
-
-class StylusCompiler {
-  constructor(cfg) {
-    if (cfg == null) cfg = {};
-    this.rootPath = cfg.paths.root;
-    this.config = cfg.plugins && cfg.plugins.stylus || {};
-    this.modules = this.config.modules || this.config.cssModules;
-    delete this.config.modules;
-    delete this.config.cssModules;
-    this._progeny = progeny({rootPath: this.rootPath});
-  }
-
-  getDependencies(file) {
-    return new Promise((resolve, reject) => {
-      this._progeny(file.path, file.data, (error, deps) => {
-        if (error) reject(error);
-        else resolve(deps);
-      });
-    });
-  }
-
-  compile(file) {
-    const data = file.data;
-    const path = file.path;
-
-    const cfg = this.config || {};
-    const compiler = stylus(data)
-      .set('filename', path)
-      .set('compress', false)
-      .set('firebug', !!cfg.firebug)
-      .set('linenos', !!cfg.linenos)
-      .set('include css', !!cfg.includeCss)
-      .include(sysPath.join(this.rootPath))
-      .include(sysPath.dirname(path))
-      .use(nib());
-
-    const defines = cfg.defines || {};
-    const paths = cfg.paths;
-    const imports = cfg.imports;
-    const plugins = cfg.plugins;
-
-    Object.keys(defines).forEach(name => {
-      compiler.define(name, defines[name]);
-    });
-
-    if (Array.isArray(paths)) {
-      paths.forEach(path => compiler.include(path));
-    }
-    if (Array.isArray(imports)) {
-      imports.forEach(relativePath => compiler.import(relativePath));
-    }
-    if (Array.isArray(plugins)) {
-      const handler = plugin => compiler.use(plugin());
-      plugins.forEach(pluginName => {
-        if (typeof pluginName === 'function') {
-          compiler.use(pluginName);
-        } else if (Array.isArray(pluginName)) {
-          const pluginModule = require(pluginName[0]);
-          handler(pluginModule[pluginName[1]]);
-        } else {
-          handler(require(pluginName));
-        }
-      });
-    }
-
-    return new Promise((resolve, reject) => {
-      compiler.render((error, data) => {
-        if (error) return reject(getError(error));
-
-        if (this.modules) {
-          cssModulify(path, data).then(resolve, reject);
-        } else {
-          resolve({data});
-        }
-      });
-    });
-  }
 }
 
 StylusCompiler.prototype.brunchPlugin = true;
 StylusCompiler.prototype.type = 'stylesheet';
 StylusCompiler.prototype.extension = 'styl';
 
+StylusCompiler.prototype.compile = function(data, path, callback) {
+  var cfg = this.config;
+  var compiler = stylus(data)
+    .set('filename', path)
+    .set('compress', false)
+    .set('firebug', !!cfg.firebug)
+    .set('linenos', !!cfg.linenos)
+    .set('include css', !!cfg.includeCss)
+    .include(sysPath.join(this.rootPath))
+    .include(sysPath.dirname(path))
+    .use(nib());
+  if (cfg !== {}) {
+    var defines = cfg.defines || {};
+    var paths = cfg.paths;
+    var imports = cfg.imports;
+    var plugins = cfg.plugins;
+    Object.keys(defines).forEach(function(name) {
+      compiler.define(name, defines[name]);
+    });
+    if (Array.isArray(paths)) {
+      paths.forEach(function(path) {
+        compiler.include(path);
+      });
+    }
+    if (Array.isArray(imports)) {
+      imports.forEach(function(relativePath) {
+        compiler['import'](relativePath);
+      });
+    }
+    if (Array.isArray(plugins)) {
+      var handler = function(plugin) {
+        compiler.use(plugin());
+      };
+      plugins.forEach(function(pluginName) {
+        if (typeof define !== 'undefined' && define.amd) {
+          require([pluginName], handler);
+        } else {
+          handler(require(pluginName));
+        }
+      });
+    }
+  }
+  compiler.render(callback);
+};
 
 module.exports = StylusCompiler;
